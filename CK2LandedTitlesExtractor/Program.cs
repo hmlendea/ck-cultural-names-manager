1b57b4e2ecf46eeef67b56d03714d9ba0541f672
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2016-11-13 11:24:49 +0200

    New project upload

---
+++ CK2LandedTitlesExtractor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CK2LandedTitlesExtractor
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
