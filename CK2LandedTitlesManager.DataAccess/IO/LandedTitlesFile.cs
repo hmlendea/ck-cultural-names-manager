0f64def674005609fdd08628442971de42f13191
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-10-11 17:33:10 +0300

    Added the Pdoxcl2Sharp package

---
+++ CK2LandedTitlesManager.DataAccess/IO/LandedTitlesFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Pdoxcl2Sharp;
+
+namespace CK2LandedTitlesManager.DataAccess.IO
+{
+    public sealed class LandedTitlesFile : IParadoxRead, IParadoxWrite
+    {
+        public void TokenCallback(ParadoxParser parser, string token)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Write(ParadoxStreamWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
