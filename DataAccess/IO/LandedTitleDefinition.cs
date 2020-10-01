7858b6a35ff813006ccfd115a080efc692f83a7f
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-10-01 16:20:17 +0300

    Removed unused title properties

--- DataAccess/IO/LandedTitleDefinition.cs
+++ DataAccess/IO/LandedTitleDefinition.cs
@@ -31,170 +31,64 @@ namespace CK2LandedTitlesManager.DataAccess.IO
 
             switch (token)
             {
-                // TODO: Implement these
+                // TODO: Skip brackets
                 case "allow":
                 case "coat_of_arms":
+                case "color":
+                case "color2":
+                case "female_names":
                 case "gain_effect":
+                case "male_names":
                 case "pagan_coa":
                     parser.ReadInsideBrackets((p) => { });
                     break;
 
-                // TODO: Implement these
+                // TODO: Skip values
+                case "assimilate":
+                case "caliphate":
                 case "can_be_claimed":
                 case "can_be_usurped":
+                case "capital":
+                case "controls_religion":
+                case "creation_requires_capital":
+                case "culture":
+                case "dignity":
+                case "dynasty_title_names":
+                case "foa":
+                case "graphical_culture":
+                case "has_top_de_jure_capital":
+                case "holy_order":
+                case "holy_site":
+                case "independent":
+                case "landless":
+                case "location_ruler_title":
+                case "mercenary_type":
+                case "mercenary":
+                case "monthly_income":
+                case "name_tier":
                 case "pentarchy":
+                case "pirate":
+                case "primary":
+                case "purple_born_heirs":
                 case "rebel":
+                case "religion":
                 case "replace_captain_on_death":
+                case "short_name":
+                case "strength_growth_per_century":
+                case "title_female":
+                case "title_prefix":
+                case "title":
                 case "top_de_jure_capital":
+                case "tribe":
                 case "used_for_dynasty_names":
                     parser.ReadString();
-                    break;
-
-                case "assimilate":
-                    LandedTitleEntity.AllowsAssimilation = parser.ReadBool();
-                    break;
-
-                case "color":
-                    LandedTitleEntity.PrimaryColour = parser.ReadIntList().ToArray();
-                    break;
-
-                case "color2":
-                    LandedTitleEntity.SecondaryColour = parser.ReadIntList().ToArray();
-                    break;
-
-                case "caliphate":
-                    LandedTitleEntity.IsCaliphate = parser.ReadBool();
-                    break;
-
-                case "capital":
-                    LandedTitleEntity.CapitalId = parser.ReadInt32();
-                    break;
-
-                case "controls_religion":
-                    LandedTitleEntity.ControlledReligionId = parser.ReadString();
-                    break;
-
-                case "creation_requires_capital":
-                    LandedTitleEntity.CreationRequiresCapital = parser.ReadBool();
-                    break;
-
-                case "culture":
-                    LandedTitleEntity.CultureId = parser.ReadString();
-                    break;
-
-                case "dignity":
-                    LandedTitleEntity.Dignity = parser.ReadInt32();
-                    break;
-
-                case "dynasty_title_names":
-                    LandedTitleEntity.UseDynastyTitleNames = parser.ReadBool();
-                    break;
-
-                case "female_names":
-                    LandedTitleEntity.FemaleNames = parser.ReadStringList();
-                    break;
-
-                case "foa":
-                    LandedTitleEntity.TitleFormOfAddress = parser.ReadString();
-                    break;
-
-                case "graphical_culture":
-                    LandedTitleEntity.GraphicalCulture = parser.ReadString();
-                    break;
-                    
-                case "has_top_de_jure_capital":
-                    LandedTitleEntity.HasTopDeJureCapital = parser.ReadBool();
-                    break;
-
-                case "holy_order":
-                    LandedTitleEntity.IsHolyOrder = parser.ReadBool();
-                    break;
-
-                case "holy_site":
-                    LandedTitleEntity.HolySites.Add(parser.ReadString());
-                    break;
-
-                case "independent":
-                    LandedTitleEntity.IsIndependent = parser.ReadBool();
-                    break;
-
-                case "landless":
-                    LandedTitleEntity.IsLandless = parser.ReadBool();
-                    break;
-
-                case "location_ruler_title":
-                    LandedTitleEntity.TitleContainsCapital = parser.ReadBool();
-                    break;
-
-                case "male_names":
-                    LandedTitleEntity.MaleNames = parser.ReadStringList();
-                    break;
-
-                case "mercenary":
-                    LandedTitleEntity.IsMercenaryGroup = parser.ReadBool();
-                    break;
-
-                case "mercenary_type":
-                    LandedTitleEntity.MercenaryType = parser.ReadString();
-                    break;
-
-                case "monthly_income":
-                    LandedTitleEntity.MonthlyIncome = parser.ReadInt32();
-                    break;
-
-                case "name_tier":
-                    LandedTitleEntity.TitleNameTierId = parser.ReadString();
-                    break;
-
-                case "pirate":
-                    LandedTitleEntity.IsPirate = parser.ReadBool();
-                    break;
-
-                case "primary":
-                    LandedTitleEntity.IsPrimaryTitle = parser.ReadBool();
-                    break;
-
-                case "purple_born_heirs":
-                    LandedTitleEntity.HasPurpleBornHeirs = parser.ReadBool();
-                    break;
-
-                case "religion":
-                    LandedTitleEntity.ReligionId = parser.ReadString();
-                    break;
-
-                case "short_name":
-                    LandedTitleEntity.UseShortName = parser.ReadBool();
-                    break;
-
-                case "strength_growth_per_century":
-                    LandedTitleEntity.StrengthGrowthPerCentury = parser.ReadFloat();
-                    break;
-
-                case "title":
-                    LandedTitleEntity.TitleLocalisationId = parser.ReadString();
-                    break;
-
-                case "title_female":
-                    LandedTitleEntity.TitleLocalisationFemaleId = parser.ReadString();
-                    break;
-
-                case "title_prefix":
-                    LandedTitleEntity.TitleLocalisationPrefixId = parser.ReadString();
-                    break;
-
-                case "tribe":
-                    LandedTitleEntity.IsTribe = parser.ReadBool();
                     break;
 
                 default:
                     string stringValue = parser.ReadString();
                     int intValue;
 
-                    if (int.TryParse(stringValue, out intValue))
-                    {
-                        LandedTitleEntity.ReligiousValues.AddOrUpdate(token, intValue);
-                    }
-                    else
+                    if (!int.TryParse(stringValue, out intValue)) // If it's not a relivious value
                     {
                         LandedTitleEntity.DynamicNames.AddOrUpdate(token, stringValue);
                     }
